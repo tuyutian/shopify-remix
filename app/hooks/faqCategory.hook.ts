df88354d0dc36e40b12ee5a04746ba519ad19ab8
maxotm <maxotm@trackingmore.org>
2025-01-07 09:23:09 +0800

    feat: 删除无用文件

--- app/hooks/faqCategory.hook.ts
+++
@@ -1,9 +0,0 @@
-import { FaqCategoryContext } from "@/contexts/FaqCategoryContext";
-import { useContext } from "react";
-
-const useFaqCategory = () => {
-    const { categoryList, setCategoryList } = useContext(FaqCategoryContext);
-    return { categoryList, setCategoryList };
-}
-
-export {useFaqCategory}
